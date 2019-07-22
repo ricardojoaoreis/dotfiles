6faf72e0162a1ecda0815f4f1e36bea1123eff5f
Ricardo Reis <ricardojoaoreis@gmail.com>
2019-07-22 14:42:52 +0100

    Add GPG recipient to YADM config Remove fish greeting Add $HOME/.local/bin to fish_user_paths Add sdocker abbreviation Set tmux terminal to $TERM variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,11 @@ switch (uname)
         set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 end
 
+# User paths
+set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
+
+# Remove fish greeting
+set fish_greeting
+
 # Abbreviations
 abbr -a -g sdocker 'docker --tls -H'
